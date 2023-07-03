dff8612c64009c7609fa30a054eb4804b809d822
Mafer <Mafer@LAPTOP-DEQ2FBLL>
2023-07-02 23:15:07 -0500

    Agregar archivos de proyecto.

---
+++ mcordovaTarea5/Plan.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace mcordovaTarea5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Plan : ContentPage
+    {
+        private string Url = "http://192.168.1.4/ws_uisrael/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<PlanAlimenticio> post;
+        public Plan()
+        {
+            InitializeComponent();
+            ObtenerPlanes();
+        }
+
+
+        private async void ObtenerPlanes()
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<PlanAlimenticio> listaPost = JsonConvert.DeserializeObject<List<PlanAlimenticio>>(contenido);
+            post = new ObservableCollection<PlanAlimenticio>(listaPost);
+            listaPlan.ItemsSource = post;
+        }
+    }
+}
---
+++ mcordovaTarea5/PlanAlimenticio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mcordovaTarea5
+{
+    internal class PlanAlimenticio
+    {
+        public  int id_palim { set; get; }
+        public  string nombre_palim { set; get;}
+        public string descripcion_palim { set; get; }
+        public int duracion_palim { set ; get; }
+        public int pesoactual_palim { set ; get; }
+        public DateTime fecha_palim { set ; get; }
+        public string estado_palim { set ; get; }
+
+    }
+}
